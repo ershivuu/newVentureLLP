9058827016794c31a5728e3c29c79aa5e7296e95
Shivam Gupta <er.shivamsg@gmail.com>
2024-07-10 17:16:44 +0530

    final code

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,1 +1,1 @@
-export const API_URL = "http://192.168.1.21:5000";
+export const API_URL = "https://corusviewventure.com/api";
--- src/Services/frontendServices.js
+++ src/Services/frontendServices.js
@@ -75,7 +75,7 @@ export const getHomeSectionThree = async () => {
 export const aboutPageBannerData = async () => {
   try {
     const response = await axios.get(`${apiUrl}/aboutus_banner`);
-    return response.data.data; // Adjust to access the 'data' array
+    return response.data.data;
   } catch (error) {
     console.error("Error fetching projects data:", error);
     throw error;
